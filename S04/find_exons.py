d6d290a5f8404b9e27664b54d6081857d39a8a43
claugce <c.gonzalezce.2025@alumnos.urjc.es>
2026-02-18 14:03:07 +0100

    SEQUENCE

---
+++ S04/find_exons.py
@@ -0,0 +1,4 @@
+from pathlib import Path
+FILENAME = "sequences/ADA_EXONS.txt"
+file_contents = Path(FILENAME).read_text()
+print(file_contents)
---
+++ S04/process_exons.py
@@ -0,0 +1,2 @@
+apoe_exons = get_exons_from_file("APOE_EXONS.txt")
+print(apoe_exons)
