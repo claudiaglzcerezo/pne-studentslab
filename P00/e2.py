9af96141b798ad9c75612ccaa091e4b95faf0c32
claugce <c.gonzalezce.2025@alumnos.urjc.es>
2026-02-23 08:57:32 +0100

    SEQUENCE

--- P00/e2.py
+++ P00/e2.py
@@ -1,4 +1,5 @@
 from Seq0 import seq_read_fasta
 if __name__ == "__main__":
     filename = input("Enter a file: ")
-    seq_read_fasta(filename)
+    name = filename + ".txt"
+    seq_read_fasta(name)
