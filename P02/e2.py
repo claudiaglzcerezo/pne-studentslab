28b0e8ef35107d4cfd987960b07ef376355d80a7
claugce <c.gonzalezce.2025@alumnos.urjc.es>
2026-03-09 21:18:28 +0100

    SEQUENCE

--- P02/e2.py
+++ P02/e2.py
@@ -2,7 +2,7 @@ from Client0 import Client
 PRACTICE = 2
 EXERCISE = 2
 print(f"-----| Practice {PRACTICE}, Exercise {EXERCISE} |------")
-IP = "212.128.255.54"
+IP = "127.0.0.1"
 PORT = 8080
 c = Client(IP, PORT)
 print(c)
